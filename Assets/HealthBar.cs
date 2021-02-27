a55c9a61cd88155b7eb108be9677d491b72576fe
max_lyakin <lyakinm@yandex.ru>
2021-02-27 19:22:51 +0400

    добавил полоску хп и методы хил дэмэдж

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image bar;
+    public float fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fill = 1f;
+    }
+    void Damage(int d,int MaxHealth)
+    {
+        fill -= d / MaxHealth;
+    }
+    void Heal(int h, int MaxHealth)
+    {
+        fill += h / MaxHealth;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        bar.fillAmount = fill;
+    }
+}
